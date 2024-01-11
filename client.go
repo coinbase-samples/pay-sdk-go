91d7c3f560c1c866b6257b6d540aa1afcbcf0ceb
Patrick Hughes <patrick.hughes@coinbase.com>
2024-01-11 14:29:07 -0600

    add client

---
+++ client.go
@@ -0,0 +1,37 @@
+package pay
+
+import (
+	"net/http"
+)
+
+var baseUrl = "https://pay.coinbase.com/api/v1/buy"
+
+type Client struct {
+	HttpClient  http.Client
+	Credentials *Credentials
+	HttpBaseUrl string
+	Host        string //new
+}
+
+func NewClient(creds *Credentials, httpClient http.Client) *Client {
+
+	return &Client{
+		HttpClient:  httpClient,
+		Credentials: creds,
+		HttpBaseUrl: baseUrl,
+		Host:        DefaultHost, //new
+	}
+}
+
+func (c *Client) SetHeaders(req *http.Request) {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("CBPAY-VERSION", "2018-03-22")
+	req.Header.Set("CBPAY-APP-ID", c.Credentials.AppId)
+	req.Header.Set("CBPAY-API-KEY", c.Credentials.ApiKey)
+}
+
+func (c *Client) BaseUrl(u string) *Client {
+	c.HttpBaseUrl = u
+	return c
+
+}
