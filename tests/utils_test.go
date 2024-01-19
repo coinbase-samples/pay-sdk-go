fb6fe0c34b159554db35f458fc6b68b27350ec4a
Patrick Hughes <patrick.hughes@coinbase.com>
2024-01-19 12:10:16 -0600

    add test for generateonrampurl

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -108,6 +109,56 @@ func TestBuildTransactionUrl(t *testing.T) {
 	if resultParsed.RawQuery != expectedUrlParsed.RawQuery {
 		t.Fatalf("unexpected query: got %s, expected %s", resultParsed.RawQuery, expectedUrlParsed.RawQuery)
 
+	}
+
+}
+
+func TestGenerateOnRampUrl(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	creds := &pay.Credentials{
+		ApiKey: os.Getenv("CBPAY_API_KEY"),
+		AppId:  os.Getenv("CBPAY_APP_ID"),
+	}
+	c := pay.NewClient(creds, http.Client{})
+
+	destinationWallets := []pay.DestinationWallet{{
+		Address:           "0xabcdef",
+		Blockchains:       &[]string{"solana", "ethereum"},
+		Assets:            &[]string{"USDC", "ETH"},
+		SupportedNetworks: &[]string{"USDC", "ethereum"}},
+	}
+
+	onRampParams := pay.OnRampAppParams{
+		DestinationWallets: destinationWallets,
+	}
+
+	p := pay.GenerateOnRampUrlOptions{
+		AppId:           c.Credentials.AppId,
+		Host:            &c.Host,
+		OnRampAppParams: onRampParams,
+	}
+
+	//expected url
+	parsedUrl, _ := url.Parse(c.Host)
+	parsedUrl.Path = "/buy/select-asset"
+	destinationWalletsJson, _ := json.Marshal(destinationWallets)
+	v := url.Values{}
+	v.Set("appId", c.Credentials.AppId)
+	v.Set("destinationWallets", string(destinationWalletsJson))
+	expectedUrl := parsedUrl.String() + "?" + v.Encode()
+
+	actualUrl, err := c.GenerateOnRampUrl(ctx, p)
+	if err != nil {
+		t.Fatalf("err %s", err)
+	}
+
+	if err != nil {
+		t.Errorf("GenerateOnRampUrl returned an error: %v", err)
+	}
+	if actualUrl != expectedUrl {
+		t.Errorf("GenerateOnRampUrl returned unexpected URL: got %v, want %v", actualUrl, expectedUrl)
 	}
 
 }
