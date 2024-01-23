41102b38edb4e86c0c05f53d32d756a656cf0620
Patrick Hughes <patrick.hughes@coinbase.com>
2024-01-22 20:25:20 -0600

    check for quoteId

--- tests/buy_test.go
+++ tests/buy_test.go
@@ -54,10 +54,46 @@ func TestBuyOptions(t *testing.T) {
 
 	response, err := c.BuyOptions(ctx, cc, &subdivision)
 	if err != nil {
-		t.Fatalf("error retrieving buyoptions")
+		t.Fatalf("error retrieving Buy Options")
 	}
 
 	if response.PaymentCurrencies == nil && response.PurchaseCurrencies == nil {
 		t.Fatalf("error buy response returned nil")
 	}
+}
+
+func TestBuyQuote(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	creds := &pay.Credentials{
+		ApiKey: os.Getenv("CBPAY_API_KEY"),
+		AppId:  os.Getenv("CBPAY_APP_ID"),
+	}
+	c := pay.NewClient(creds, http.Client{})
+	network := "Bitcoin"
+	subdivision := "NY"
+	payload := &pay.BuyQuotePayload{
+		PurchaseCurrency: "BTC",
+		PurchaseNetwork:  &network,
+		PaymentAmount:    "100",
+		PaymentCurrency:  "USD",
+		PaymentMethod:    "CARD",
+		Country:          "US",
+		Subdivision:      &subdivision,
+	}
+
+	response, err := c.BuyQuote(ctx, payload)
+	if err != nil {
+		t.Fatalf("error retrieving Buy Quote: %s", err)
+	}
+
+	if response.QuoteId == "" {
+		t.Fatalf("buy quote returned no quoteId")
+	}
+
+}
+
+func TestTransactioNStatus(t *testing.T) {
+
 }
