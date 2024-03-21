257bc634b489f6db86c290f5643927b0d1e7609a
Patrick Hughes <patrick.hughes@coinbase.com>
2024-03-21 12:58:26 -0500

    add copyright

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -1,7 +1,20 @@
-package main
+/**
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//fix error messages
-//fix variable names like resp, v, cc, countryCode, confirmUrl
+package main
 
 import (
 	"context"
@@ -44,7 +57,6 @@ func TestSetOptionsParams(t *testing.T) {
 
 func TestSetOptionsWithSubdivision(t *testing.T) {
 
-	//Arrange
 	countryCode := "US"
 	subdivision := "NY"
 	confirmUrl := fmt.Sprintf("https://pay.coinbase.com/api/v1/buy/options?country=%s&subdivision=%s", countryCode, subdivision)
